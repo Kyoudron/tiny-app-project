01e50506f50e4d3d07c81ec360b4cefedfcf6e10
Kapish <kapish.k.1@gmail.com>
2017-01-19 19:50:12 -0500

    added user registeration

--- express_server.js
+++ express_server.js
@@ -1,13 +1,14 @@
-var express = require("express");
-var cookieParser = require("cookie-parser")
+const express = require("express");
+const cookieParser = require("cookie-parser")
+const bodyParser = require("body-parser");
 
-var app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+const app = express();
 app.use(cookieParser())
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
@@ -19,19 +20,60 @@ function generateRandomString(length, sixChars) {
   return result
 }
 
-// var randomString = generateRandomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-
+function generateRandomID(length, allID) {
+  var result = '';
+  for (var i = length; i > 0; --i)
+    result += allID[Math.floor(Math.random() * allID.length)];
+  return result
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-// app.get("/login", (req, res) =>{
-//   res.cookie("username", "username");
-//   res.render("_header");
-// });
+// let randomID = generateRandomID(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+var userDatabase = {
+    id: {
+      id: generateRandomID(4, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+      email: "kapish.k.1@gmail.com",
+      password: "lighthouselabs"
+  }
+}
 
+//
+
+app.get("/register", (req, res)=> {
+  res.render("register")
+});
+
+app.post("/register", (req, res) => {
+  let userExists = false
+  let randomID = (generateRandomID(4, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+for (var newUserVar in userDatabase) {
+  if (userDatabase[newUserVar].email === req.body.email) {
+    // console.log("it matched")
+    userExists = true
+  }
+}
+  if (userExists) {
+    res.status(400).send("Email user already exists!")
+
+  } else if (req.body.email === "" || req.body.password === "") {
+  res.status(400).send("Email and/or password were left blank")
+
+} else {
+  res.cookie("userEmail", req.body.email)
+  const newUser = {
+    id: randomID,
+    email: req.body.email,
+    password: req.body.password,
+    }
+    userDatabase[randomID] = newUser
+    res.redirect("/urls")
+  }
+})
 
 //
 
@@ -53,9 +95,30 @@ app.post("/urls/:id/delete", (req, res) => {
 //
 
 
-app.post("/login", (req, res) => {
+
+
+app.get("/login", (req, res) => {
   res.cookie("username", req.body.username)
-  res.redirect("/urls")
+  res.render("urls_login")
+})
+
+
+app.post("/login", (req, res) => {
+  let foundUser = null
+  for(let userID in userDatabase) {
+    if (userDatabase[userID].email === req.body.email) {
+      foundUser = userDatabase[userID];
+      break;
+    }
+  }
+  if(!foundUser) {
+    return res.status(403).send("User not found");
+  }
+  if(foundUser.password !== req.body.password){
+    return res.status(403).send("Not the right password :(")
+  }
+  res.cookie("user_id", req.body.email)
+  res.redirect(`/urls`)
 });
 
 
@@ -112,13 +175,6 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 
-//
-
-// app.use(function(req, res){
-//   res.status(404).send("Sorry, not found!");
-// });
-
-
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -136,3 +192,14 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+// bcrypt.hach(req.body/password, 10, (err, hash)=> {
+//   if (err) {
+//     res.send ("error")
+//     return
+//   }
+// })
+
+// data.users.push()
